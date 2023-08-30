d614ff4d83f986ff7818474392c8e335d195575c
Robert Michael González Núñez <71914244+ROBERTG96@users.noreply.github.com>
2023-08-30 17:33:59 -0500

    Folder

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ import cors from "cors";
 import morgan from "morgan";
 import ecommerceRouter from "./router/ecommerceRouter.js";
 
+// Mongoose
+import "./database/mongoose.js";
+
 const serverApp = express(); // SERVER EXPRESS
 const PORT = process.env.PORT || 8000; // SET PORT
 
@@ -14,7 +17,6 @@ serverApp.use(cors()); // Cors Other Origins
 serverApp.use(morgan("dev")); // Get Http Log
 
 // Router
-
 serverApp.use("/api", ecommerceRouter);
 
 // Server Listen
---
+++ models/User.js
@@ -0,0 +1,17 @@
+import { Schema, model } from "mongoose";
+
+let collection = "users";
+let schema = new Schema(
+  {
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+    photo: { type: String, required: true },
+    role: { type: Number, default: 0 }
+  },
+  {
+    timestamps: true,
+  }
+);
+
+let User = model(collection, schema);
+export default User;
