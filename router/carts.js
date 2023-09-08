82936bf339289b52dcddf42cb286345d33a9032c
Guillermo Duque <guille980627@gmail.com>
2023-09-08 16:45:13 -0500

    update enrouted

--- router/carts.js
+++ router/carts.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
-import read from '../controllers/carts/read.js'
 import create from "../controllers/carts/create.js";
+import read from '../controllers/carts/read.js'
+import update from '../controllers/carts/update.js'
 
 let cartRouter = Router();
 
 cartsRouter.get('/:id', read);
 cartsRouter.post('/', create);
+cartsRouter.put('/', update);
 
 export default cartRouter;
