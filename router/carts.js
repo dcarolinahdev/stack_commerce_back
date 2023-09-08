4643bce5afb7e76a18debb4e8fe8079b6dd4c501
Guillermo Duque <guille980627@gmail.com>
2023-09-08 16:42:50 -0500

    read enrouted

--- router/carts.js
+++ router/carts.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
+import read from '../controllers/carts/read.js'
 import create from "../controllers/carts/create.js";
 
 let cartRouter = Router();
 
+cartsRouter.get('/:id', read);
 cartsRouter.post('/', create);
 
 export default cartRouter;
