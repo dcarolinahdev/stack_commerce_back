8d9824dc729128208e90ba5e9f2e5b03c1c8a617
Guillermo Duque <guille980627@gmail.com>
2023-09-08 16:31:57 -0500

    cart router

--- controllers/carts/create.js
+++ controllers/carts/create.js
@@ -2,7 +2,7 @@ import Cart from '../../models/Cart.js'
 
 export default async function ( req,res,next ) {
     try {
-        let one = await Cart.create(req.body);
+        await Cart.create(req.body);
         return res.status(201).json({
             success: true,
             response: "Product added to cart!"
