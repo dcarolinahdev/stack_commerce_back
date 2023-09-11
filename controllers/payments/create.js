66d3be839127818df913c4084c6108daf4e70e70
Diana Carolina Hernandez Abahonza <dianacaro3552@gmail.com>
2023-09-11 16:20:02 -0500

    Adding try catch worked in class

--- controllers/payments/create.js
+++ controllers/payments/create.js
@@ -1,16 +1,30 @@
 import mercadopago from 'mercadopago'
 
-    export default async(req, res) => {
-        mercadopago.configure({ access_token: process.env.MPAY_SECKEY })
-        let preference = {
-            items: req.body.products, //array de productos que se deben pagar
-            back_urls : {
-                success : process.env.CLIENT_URL + "/payment/success" ,
-                failure : process.env.CLIENT_URL + "/payment/failure" ,
-                pending : process.env.CLIENT_URL + "/payment/pending"
-            },
-        };
+    export default async(req, res, next) => {
+        try {
+            mercadopago.configure({ access_token: process.env.MPAY_SECKEY })
 
-        let response = await mercadopago.preferences.create(preference)
-        return res.status(201).redirect(response.body.init_point);
+            let preference = {
+                items: [
+                    {
+                        title:'Producto 1',
+                        quantity:3,
+                        currency_id:'COP',
+                        unit_price:2500
+                    }
+                ],
+                // items: req.body.products, //array de productos que se deben pagar
+                back_urls : {
+                    success : process.env.CLIENT_URL + "/payment/success" ,
+                    failure : process.env.CLIENT_URL + "/payment/failure" ,
+                    pending : process.env.CLIENT_URL + "/payment/pending"
+                },
+            };
+
+            let response = await mercadopago.preferences.create(preference)
+            console.log(response)
+            return res.status(201).redirect(response.body.init_point);
+        } catch (error) {
+            return next(error)
+        }
     }
